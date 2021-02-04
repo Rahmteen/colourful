931bead95ea9c9ee25cbc85e2bf870d687898f8c
Ramtin Khoee <ramtinkhoee@Ramtins-MacBook-Pro.local>
2021-02-03 23:52:46 -0800

    day 1 progress

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-const bodyTag = document.getElementsByTagName("BODY")[0]
+const bodyTag = document.getElementsByTagName('BODY')[0];
+function makeRed(){
+    bodyTag.setAttribute('style', `background-color: red !important`, );
+}
+// function randomColor() {
+//     return `rgb ${Math.floor(Math.random(0,255))} + ', ' + ${Math.floor(Math.random(0,255))} + ', ' +  ${Math.floor(Math.random(0,255))}`;
+//   }
 
-bodyTag.style.backgroundColor = 'grey'
+// const newColor = 'yellow'; 
+
+console.log(newColor);
+object.addEventListener("click", myScript);
+
+var colors = ['green', 'orange', 'blue']; // Define Your colors here, can be html name of color, hex, rgb or anything what You can use in CSS
+var active = 0;
+setInterval(function(){
+    document.querySelector('body').style.background = colors[active];
+    active++;
+    if (active == colors.length) active = 0;
+}, 30000);
+
+
+// bodyTag.setAttribute('style', `background-color: red !important`, );
+// bodyTag.setAttribute('style', `background: red !important`);
